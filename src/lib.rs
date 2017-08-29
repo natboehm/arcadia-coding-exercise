7bf9158c5978b924688799afdf20185d3a23678e
Natalie Boehm <nmb56@pitt.edu>
2017-08-28 21:28:26 -0400

    basic blog library structure

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,73 @@
+struct Blog {
+    title: String,
+    posts: Vec<Post>,
+}
+
+struct Post {
+    title: String,
+    author: String,
+    body: String,
+    labels: Option<Vec<String>>,
+    comments: Option<Vec<Comment>>,
+}
+
+struct Comment {
+    author: String,
+    body: String,
+}
+
+impl Post {
+    fn create() {
+
+    }
+
+    fn read() {
+
+    }
+
+    fn update() {
+
+    }
+
+    fn delete() {
+
+    }
+}
+
+impl Comment {
+    fn create() {
+
+    }
+
+    fn read() {
+
+    }
+
+    fn update() {
+
+    }
+
+    fn delete() {
+
+    }
+}
+
+fn search_by_title() {
+
+}
+
+fn search_by_author() {
+
+}
+
+fn search_by_label() {
+
+}
+
+fn search_by_body() {
+
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
