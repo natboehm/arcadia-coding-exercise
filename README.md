c8ef4d8334eead9f111ef7c67738db3e5dfc5a0d
Natalie Boehm <nmb56@pitt.edu>
2017-08-31 22:39:24 -0400

    very limited functionality + README

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
-Here lies the code for a blogging library
+##Arcadia Coding Exercise
+
+I implemented the beginnings of the blogging Library in Rust. The library compiles,
+however I did not have the time to complete much functionality or a main function
+to demonstrate functionality.
+
+Rust version: 1.19
+
+To compile: `Cargo Build`
+
+To run: `Cargo run`
+
+(Running will only print 'hello world')
+
+When compiled there will be numerous warnings as output, they're just letting
+you know that the variables are not used. This is something I really like
+about the Rust compiler, that it tells you where you can have dead code on
+compilation. On crates.io these are enforced as compiler errors in order to
+keep the code as clean as possible.
+
+The library was designed to have three main structs, one representing the Blog,
+one representing a post, another representing a comment. A blog has many posts,
+a post has many comments. The blog should have been implemented in the `impl`
+block, and called by a main function in `src/main.rs`.
+
+The search functions would have been implemented alongside the blog, and would
+have been useful in the CRUD functionality for the app. To update the author,
+body, or labels, we would have had to search for the old and replace it with
+the new.
+
+Unit tests would have been written at the bottom of `src/lib.rs`. These would
+have tested each function, ideally each function returning something to check
+against.
--- src/lib.rs
+++ src/lib.rs
@@ -1,70 +1,111 @@
+#[derive(Clone)]
 struct Blog {
     title: String,
     posts: Vec<Post>,
 }
 
+#[derive(Clone)]
 struct Post {
     title: String,
     author: String,
     body: String,
-    labels: Option<Vec<String>>,
-    comments: Option<Vec<Comment>>,
+    labels: Vec<String>,
+    comments: Vec<Comment>,
 }
 
+#[derive(Clone)]
 struct Comment {
     author: String,
     body: String,
 }
 
-impl Post {
-    fn create() {
+impl Blog {
+    fn init_blog(title: String) -> Blog {
+        let post_list = Vec::new();
+
+        let new_blog = Blog {
+            title: title,
+            posts: post_list,
+        };
+
+        return new_blog
+    }
+
+    fn create_post(mut self, title: String, author: String, body: String, labels: Vec<String>, comments: Vec<Comment>) {
+        let new_post = Post {
+            title: title,
+            author: author,
+            body: body,
+            labels: labels,
+            comments: comments,
+        };
+
+        self.posts.push(new_post);
+    }
+
+    fn read_posts(self) -> Vec<Post> {
+        return self.posts;
+    }
+
+    fn read_post(self, title: String) -> Option<Post> {
+        // if let statement - if pattern returned by search_by_title
+        // matches Some(post), if evaluates to true
+        // Some(post) is evaluating if there is a value in post,
+        // or is post equal to None
+        if let Some(post) = search_by_title(self, title) {
+            return Some(post);
+        } else {
+            return None;
+        }
+    }
+
+    fn update_title(self, old_title: String, new_title: String) {
+        if let Some(post) = search_by_title(self.clone(), old_title) {
+            // do update something like find old post, delete post,
+            // reinsert post with new title
+            // return some form of success statement
+            self.delete_post();
+        } else {
+            // post to be updated was not found, return
+            // some sort of error statement
+        }
+    }
+
+    fn update_author() {
 
     }
 
-    fn read() {
+    fn update_body() {
 
     }
 
-    fn update() {
+    fn update_labels() {
 
     }
 
-    fn delete() {
+    fn delete_post(self) {
 
     }
 }
 
-impl Comment {
-    fn create() {
-
+fn search_by_title(blog: Blog, title: String) -> Option<Post> {
+    for post in blog.posts {
+        if title == post.title {
+            return Some(post);
+        }
     }
-
-    fn read() {
-
-    }
-
-    fn update() {
-
-    }
-
-    fn delete() {
-
-    }
+    return None;
 }
 
-fn search_by_title() {
+fn search_by_author(author: String) {
 
 }
 
-fn search_by_author() {
+fn search_by_label(label: String) {
 
 }
 
-fn search_by_label() {
-
-}
-
-fn search_by_body() {
+fn search_by_body(body: String) {
 
 }
 
